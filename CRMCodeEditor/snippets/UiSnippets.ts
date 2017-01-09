b9378a74a26460ef19ee749b199f4532ca312137
Jason Lattimer <jason.lattimer@gmail.com>
2017-01-08 21:49:37 -0600

    Added missing quickform snippets

--- CRMCodeEditor/snippets/UiSnippets.ts
+++ CRMCodeEditor/snippets/UiSnippets.ts
@@ -42,7 +42,64 @@
 		collection.push(new Snippet("XrmUiSetFormNotification",
 			monaco.languages.CompletionItemKind.Function,
 			"6.0+ Use this method to display form level notifications.",
-			"Xrm.Page.ui.setFormNotification(\"message\", \"ERROR|WARNING|INFO\", \"uniqueId\")"));
+            "Xrm.Page.ui.setFormNotification(\"message\", \"ERROR|WARNING|INFO\", \"uniqueId\")"));
+
+        collection.push(new Snippet("XrmUiQuickFormControls",
+            monaco.languages.CompletionItemKind.Function,
+            "8.1+ A collection of all quick view controls on a form.",
+            "Xrm.Page.ui.quickForms"));
+
+        collection.push(new Snippet("XrmUiQuickFormControlsForEach",
+            monaco.languages.CompletionItemKind.Function,
+            "8.1+ Apply an action in a delegate function to each object in the collection of quick view controls on a form.",
+            "Xrm.Page.ui.quickForms.forEach(function (qvcontrol, index) {\n" +
+            "\n" +
+            "})"));
+
+        collection.push(new Snippet("XrmUiQuickFormGetControl",
+            monaco.languages.CompletionItemKind.Function,
+            "8.1+ Gets the constituent controls in a quick view control.",
+            "Xrm.Page.ui.quickForms.get(\"qvcontrolname\").getControl(\"controlname\")"));
+
+        collection.push(new Snippet("XrmUiQuickFormGetControlType",
+            monaco.languages.CompletionItemKind.Function,
+            "8.1+ Returns a string value that categorizes quick view controls.",
+            "Xrm.Page.ui.quickForms.get(\"qvcontrolname\").getControlType()"));
+
+        collection.push(new Snippet("XrmUiQuickFormGetName",
+            monaco.languages.CompletionItemKind.Function,
+            "8.1+ Returns the name assigned to the quick view control.",
+            "Xrm.Page.ui.quickForms.get(\"qvcontrolname\").getName()"));
+
+        collection.push(new Snippet("XrmUiQuickFormGetParent",
+            monaco.languages.CompletionItemKind.Function,
+            "8.1+ Returns a reference to the section object that contains the control.",
+            "Xrm.Page.ui.quickForms.get(\"qvcontrolname\").getParent()"));
+
+        collection.push(new Snippet("XrmUiQuickFormGetVisible",
+            monaco.languages.CompletionItemKind.Function,
+            "8.1+ Returns a value that indicates whether the quick view control is currently visible.",
+            "Xrm.Page.ui.quickForms.get(\"qvcontrolname\").getVisible()"));
+
+        collection.push(new Snippet("XrmUiQuickFormGetLabel",
+            monaco.languages.CompletionItemKind.Function,
+            "8.1+ Returns the label for the quick view control.",
+            "Xrm.Page.ui.quickForms.get(\"qvcontrolname\").getLabel()"));
+
+        collection.push(new Snippet("XrmUiQuickFormSetLabel",
+            monaco.languages.CompletionItemKind.Function,
+            "8.1+ Sets the label for the quick view control.",
+            "Xrm.Page.ui.quickForms.get(\"qvcontrolname\").setLabel(\"value\")"));
+
+        collection.push(new Snippet("XrmUiQuickFormIsLoaded",
+            monaco.languages.CompletionItemKind.Function,
+            "8.1+ Returns whether the data binding for the constituent controls in a quick view control is complete.",
+            "Xrm.Page.ui.quickForms.get(\"qvcontrolname\").isLoaded()"));
+
+        collection.push(new Snippet("XrmUiQuickFormRefresh",
+            monaco.languages.CompletionItemKind.Function,
+            "8.1+ Refreshes the data displayed in a quick view control.",
+            "Xrm.Page.ui.quickForms.get(\"qvcontrolname\").refresh()"));
 
 		return collection;
 	}
